df7056dee65f07aa860fde1fbc5467e6d7bac94b
jieyuz-sf <jieyu.zhang@salesforce.com>
2024-10-01 16:21:38 -0700

    Update eval_all.sh

--- mantis/benchmark/eval_all.sh
+++ mantis/benchmark/eval_all.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#. /export/share/jieyu/.bashrc
+
+export HF_HOME=/export/share/jieyu/cache/
+
+
+## activate llava environment
+source /export/share/zixianma/miniconda/bin/activate
+conda activate mantis
+
+cd /export/share/jieyu/Mantis/mantis/benchmark
 
 # for m in v4-0.05b  v4-0.1b v4-0.2b v4-both-0.05b  v4-both-0.2b  v4-both-0.1b  v4-mc-0.05b  v4-mc-0.2b v4-mc-0.1b
 for m in v4-mc-0.1b
