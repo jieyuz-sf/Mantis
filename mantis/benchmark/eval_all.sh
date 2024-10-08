c8743e81b672d8c41c064c3a70b378c62348e410
jieyuz-sf <jieyu.zhang@salesforce.com>
2024-10-07 19:14:36 -0700

    Update eval_all.sh

--- mantis/benchmark/eval_all.sh
+++ mantis/benchmark/eval_all.sh
@@ -10,7 +10,8 @@ conda activate mantis
 
 cd /export/share/jieyu/Mantis/mantis/benchmark
 
-for m in v4-0.05b  v4-0.1b v4-0.2b v4-both-0.05b  v4-both-0.2b  v4-both-0.1b  v4-mc-0.05b  v4-mc-0.2b v4-mc-0.1b
+# for m in v4-0.05b  v4-0.1b v4-0.2b v4-both-0.05b  v4-both-0.2b  v4-both-0.1b  v4-mc-0.05b  v4-mc-0.2b v4-mc-0.1b
+for m in v4-0.5b v4-both-0.5b v4-mc-0.5b
 do
   bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
 done
