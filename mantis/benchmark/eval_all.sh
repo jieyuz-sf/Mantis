ff8c0e5228c66c9088da20afe33b299bbfdc5a5c
jieyuz-sf <jieyu.zhang@salesforce.com>
2024-10-16 19:01:07 -0700

    Update eval_all.sh

--- mantis/benchmark/eval_all.sh
+++ mantis/benchmark/eval_all.sh
@@ -13,17 +13,22 @@ cd /export/share/jieyu/Mantis/mantis/benchmark
 # for m in v4-0.05b  v4-0.1b v4-0.2b v4-both-0.05b  v4-both-0.2b  v4-both-0.1b  v4-mc-0.05b  v4-mc-0.2b v4-mc-0.1b
 # for m in v4-0.5b v4-both-0.5b v4-mc-0.5b
 
-for m in v4-sz-0.05b  v4-sz-0.1b v4-sz-0.2b v4-sz-0.5b
+for m in v4-all-both-0.2b v4-all-both-sz-0.2b
 do
   bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
 done
 
-for m in v4-both-sz-0.05b  v4-both-sz-0.1b  v4-both-sz-0.2b  v4-both-sz-0.5b
-do
-  bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
-done
+# for m in v4-sz-0.05b  v4-sz-0.1b v4-sz-0.2b v4-sz-0.5b
+# do
+#   bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
+# done
 
-for m in v4-mc-sz-0.05b  v4-mc-sz-0.1b  v4-mc-sz-0.2b  v4-mc-sz-0.5b
-do
-  bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
-done
+# for m in v4-both-sz-0.05b  v4-both-sz-0.1b  v4-both-sz-0.2b  v4-both-sz-0.5b
+# do
+#   bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
+# done
+
+# for m in v4-mc-sz-0.05b  v4-mc-sz-0.1b  v4-mc-sz-0.2b  v4-mc-sz-0.5b
+# do
+#   bash eval_single_model.sh /export/share/jieyu/mantis_ckpt/Mantis-8B-siglip-llama3-pretraind/$m/checkpoint-final
+# done
